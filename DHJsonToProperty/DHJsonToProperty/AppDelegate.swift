492072a04cc839a376516a688f040c13bb1bfb52
houdonghui <houdonghui@houdonghui.com>
2019-11-05 11:57:42 +0800

    1

---
+++ DHJsonToProperty/DHJsonToProperty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DHJsonToProperty
+//
+//  Created by 候东辉 on 2019/11/5.
+//  Copyright © 2019 候东辉. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DHJsonToProperty/DHJsonToProperty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DHJsonToProperty
+//
+//  Created by 候东辉 on 2019/11/5.
+//  Copyright © 2019 候东辉. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
